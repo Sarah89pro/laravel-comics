8dbaabf77cedbab2fe866c055a877edcd791ed24
Sarah89Pro <sarahproietti1@gmail.com>
2021-06-09 01:29:05 +0200

    news

---
+++ resources/views/news.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.main')
+
+@section('content')
+
+<!--News Section-->
+<main>
+    <div class="container">
+        <div class="center">
+            <video autoplay loop>
+                <source src="{{ asset('img/Obi-Wan Says Hello There.mp4') }}" type="video/mp4">
+            </video>
+        </div>
+        
+    </div>
+</main>
+
+@endsection
