87653cdd3f15d194f00566400ef3f245db32e0c3
Sarah89Pro <sarahproietti1@gmail.com>
2021-06-09 00:52:15 +0200

    .

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,7 +4,7 @@
         <div class="img">
             <img src="{{ asset('img/dc-logo.png')}}" alt="Logo DC">
         </div>
-        <nav class="header-nav">
+        <nav class="menu">
             <ul>
                 <li><a class="active" href="{{ route('home')}}">Comics</a></li>
                 <li><a href="{{ route('news')}}"></a>News</li>
