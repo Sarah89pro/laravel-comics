3bde0272bacc9450d569978110674cafdbdfa66b
Sarah89Pro <sarahproietti1@gmail.com>
2021-06-09 01:38:12 +0200

    ok

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -6,8 +6,9 @@
 <main>
     <div class="container">
         <div class="center">
-            <video autoplay loop>
+            <video autoplay controls loop>
                 <source src="{{ asset('img/Obi-Wan Says Hello There.mp4') }}" type="video/mp4">
+                <source src="{{ asset('img/Obi-Wan Says Hello There.mp4') }}"type="video/webm">
             </video>
         </div>
         
